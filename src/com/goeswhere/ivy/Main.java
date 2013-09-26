9c50bf79998850f1c30632dc87f96cd3626be337
Chris West (Faux) <git@goeswhere.com>
2013-09-26 23:28:09 +0100

    automated cleanups

--- src/com/goeswhere/ivy/Main.java
+++ src/com/goeswhere/ivy/Main.java
@@ -1,17 +1,15 @@
-package com.goeswhere;
+package com.goeswhere.ivy;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.context.annotation.*;
-import org.springframework.boot.context.embedded.*;
-import org.springframework.boot.context.embedded.jetty.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 @ComponentScan
-@Configuration
 public class Main {
 
     @RequestMapping("/")
